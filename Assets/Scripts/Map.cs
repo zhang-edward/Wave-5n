343660ac529bc12fd7ef806d44197d78619b479a
Edward Zhang <edwardz123@gmail.com>
2016-07-10 20:13:36 -0700

    Abstract map generation from Map class
    MapGenerator now renamed to CAGenerator
    Add MapGenerator class which handles generation of IDMaps

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -25,35 +25,49 @@ public class Map : MonoBehaviour
 
 	public GameObject terrainPrefab;
 	public Sprite[] terrainSprites;
-	public GameObject[] terrainObjects;
+	public GameObject[] terrainObjectPrefabs;
 
-	private SpriteRenderer[,] terrainSpriteMap;
+	private SpriteRenderer[,] terrainSpriteMap = new SpriteRenderer[size, size];
+	private List<GameObject> terrainObjects = new List<GameObject>();
 
-	private MapGenerator mapGenerator = new MapGenerator ();
+	private MapGenerator mapGenerator = new MapGenerator(size);
 
-	[Range(0.4f, 1f)]
-	public float prob;
-	[Range(0, 10)]
-	public int noiseReduceReps;
+	public const int size = 10;
 
-	private int[,] terrain;
-	public int size;
-	public float minPercent;
 
 	void Awake()
 	{
-		InitMap ();
+		InitSpriteMap ();
+		GenerateMap ();
 	}
 
-	void InitMap()
+	void GenerateMap()
 	{
-		terrain = new int[size, size];
-		terrainSpriteMap = new SpriteRenderer[size, size];
-
-		terrain = mapGenerator.Overlay (terrain, 1, 0, prob, noiseReduceReps, true, minPercent);
-		for (int x = 0; x < terrain.GetLength(1); x ++)
+		mapGenerator.GenerateMap ();
+		ClearTerrainObjects ();
+		for (int x = 0; x < size; x ++)
 		{
-			for (int y = 0; y < terrain.GetLength(0); y ++)
+			for (int y = 0; y < size; y ++)
+			{
+				terrainSpriteMap[y, x].sprite = terrainSprites [mapGenerator.Terrain [y, x]];
+
+				if (mapGenerator.TerrainObjects[y, x] > 0)
+				{
+					//Debug.Log ("Terrain Objects matrix: " + terrainObjects [y, x]);
+					GameObject obj = Instantiate (terrainObjectPrefabs[mapGenerator.TerrainObjects[y, x] - 1]);
+					obj.transform.SetParent (this.transform);
+					obj.transform.position = new Vector2 (x, y);
+					terrainObjects.Add (obj);
+				}
+			}
+		}
+	}
+
+	private void InitSpriteMap()
+	{
+		for (int x = 0; x < size; x++)
+		{
+			for (int y = 0; y < size; y++)
 			{
 				GameObject o = Instantiate (terrainPrefab);
 				o.transform.SetParent (this.transform);
@@ -61,29 +75,21 @@ public class Map : MonoBehaviour
 
 				SpriteRenderer sr = o.GetComponent<SpriteRenderer> ();
 				terrainSpriteMap [y, x] = sr;
-				sr.sprite = terrainSprites [terrain [y, x]];
 			}
 		}
 	}
 
-	void GenerateNewMap()
+	private void ClearTerrainObjects()
 	{
-		terrain = new int[size, size];
-		terrain = mapGenerator.Overlay (terrain, 1, 0, prob, noiseReduceReps, true, minPercent);
-		for (int x = 0; x < terrain.GetLength(1); x ++)
-		{
-			for (int y = 0; y < terrain.GetLength(0); y ++)
-			{
-				terrainSpriteMap[y, x].sprite = terrainSprites [terrain [y, x]];
-			}
-		}
+		foreach (GameObject o in terrainObjects)
+			Destroy (o);
 	}
 
 	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.R))
 		{
-			GenerateNewMap ();
+			GenerateMap ();
 		}
 
 /*		if (Input.GetMouseButtonDown(0))
---
+++ Assets/TerrainObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainObject : MonoBehaviour {
+
+	public bool flippable;
+
+	// Use this for initialization
+	void Awake () {
+		// randomly flips this sprite horizontally on creation
+		if (flippable)
+			GetComponent<SpriteRenderer> ().flipX = Random.value < 0.5f;
+	}
+}
