41940ae530af48ab28d63b552b9cf63a469aa975
Edward Zhang <edwardz123@gmail.com>
2018-06-27 20:11:15 -0700

    Tweak Pawn UI

---
+++ WaveRush/Assets/Scripts/Game/StatData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Player/StatData", order = 0)]
+public class StatData : ScriptableObject {
+	
+	public const int STR  = 0;
+	public const int VIT  = 1;
+	public const int CHG  = 2;
+	public const int DEX  = 3;
+	public const int CRIT = 4;
+	public const int LUCK = 5;
+
+	[Header("Descriptions")]
+	[TextArea]
+	public string[] statDescriptions;
+}
